fef17fd4c4935bf1144a1d0f58fd8ae36943dae6
Daniel Chen <chendaniely@gmail.com>
2020-04-15 01:13:42 -0400

    dockerfile and plumber.R file

---
+++ .notes/plumber.R
@@ -0,0 +1,44 @@
+mod <- readRDS("model.RDS")
+
+#* Predict a diamond price
+#* @param new_dat new data
+#* @get /predict
+predict_price <- function(new_dat) {
+  one_row <- as.data.frame(jsonlite::fromJSON(new_dat))
+
+  return(
+    list(
+      price = predict.lm(object = mod, newdata = one_row)
+    )
+  )
+}
+
+#* Predict a diamond price given separate values
+#* @param carat
+#* @param cut
+#* @param color
+#* @param clarity
+#* @param depth
+#* @param table
+#* @param x
+#* @param y
+#* @param z
+#* @get /predict2
+predict_price2 <- function(carat, cut, color, clarity, depth, table, x, y, z) {
+  dat <- data.frame(
+    carat = as.numeric(carat),
+    cut = as.character(cut),
+    color = as.character(color),
+    clarity = as.character(clarity),
+    depth = as.numeric(depth),
+    table = as.numeric(table),
+    x = as.numeric(x),
+    y = as.numeric(y),
+    z = as.numeric(z)
+  )
+  return(
+    list(
+      price = predict.lm(mod, dat)
+    )
+  )
+}
